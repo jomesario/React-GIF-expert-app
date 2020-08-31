9bc88bdcc2ec396c0a446fc0deb68691b685caf1
Jose Mario <josemarioespinozar@gmail.com>
2020-08-31 11:53:02 -0500

    DOCS agregado

---
+++ docs/precache-manifest.9ef513bed6e784673c0320ee0ac813bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29a7915779b5738e0bb5349dace17e25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44489ffdf59797cca9a1",
+    "url": "/static/css/main.1bb26f8d.chunk.css"
+  },
+  {
+    "revision": "92a5aaf936af7a3a2930",
+    "url": "/static/js/2.7c8d2b56.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7c8d2b56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44489ffdf59797cca9a1",
+    "url": "/static/js/main.1e47d7ae.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
